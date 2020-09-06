43f122bc1e6b73b0e51c0a8160a47f066d935fb8
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2020-09-06 21:17:13 +0200

    chore(build): it is now possible to give a file to build as argument

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ source env.source.sh
 
 mkdir -p target/stl
 
-find . \( -type d -name 'templates' -prune \) -o \( -name '*.scad' -print \) | while read f; do
+{
+    if [ $# -gt 0 ]; then
+        for f in "$*"; do echo "$f"; done
+    else
+        find . \( -type d -name 'templates' -prune \) -o \( -name '*.scad' -print \)
+    fi
+} | while read f; do
     name="$(basename "$f" .scad)"
     tgt_file=target/stl/"$name".stl
     echo "'$f' -> '$tgt_file'"
